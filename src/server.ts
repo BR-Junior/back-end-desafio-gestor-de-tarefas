41fdb8c94b0fc0c5ab49d929f8c93e1a047b8a15
BR-Junior <bianor.tech@gmail.com>
2023-05-01 22:48:44 -0300

    faeat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import type from 'express';
+import cors from 'cors';
 import { router } from './routes';
 import 'dotenv/config';
 
 
 const server = type();
+
+server.use(cors());
 
 server.use(type.json());
 
