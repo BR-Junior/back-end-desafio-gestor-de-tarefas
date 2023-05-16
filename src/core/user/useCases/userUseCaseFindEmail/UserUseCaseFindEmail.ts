eca1ec7f0673678e64d01e28fad1b93ade427013
BR-Junior <bianor.tech@gmail.com>
2023-05-16 19:22:26 -0300

    refactor: add return token UserUseCaseFindEmail.ts

--- src/core/user/useCases/userUseCaseFindEmail/UserUseCaseFindEmail.ts
+++ src/core/user/useCases/userUseCaseFindEmail/UserUseCaseFindEmail.ts
@@ -5,22 +5,19 @@ import {JWTService} from '../../../../shared/services/JWTService';
 
 export class UserUseCaseFindEmail implements IUserUseCaseFindEmail{
   constructor(private repoUsecase: IUserUseCaseFindEmail) {}
-  async findEmail(params: IUserUseCaseFindEmail.Params): Promise< IUserUseCaseFindEmail.Result | Error> {
+  async findEmail(params: IUserUseCaseFindEmail.Params): Promise<IUserUseCaseFindEmail.Result | Error> {
     const response = await this.repoUsecase.findEmail(params);
-    if (response instanceof Error) return new Error('Invalid email or password');
 
+    if (response instanceof Error) return new Error(response.message);
+
+    if (!response.password) return new Error('Internal service error');
     const passwordMatch = await passwordCrypto.verifyPassword(params.password, response.password);
-    if (!passwordMatch) return  new Error('Invalid email or password');
 
-    return response;
-
-    // const accessToken = await JWTService.sign({uid: response.id});
-    // if (!passwordMatch) {
-    //   return new Error('Invalid email or password');
-    // }else {
-    //   const result = {token: accessToken, id: response.id};
-    //   return result;
-    // }
-    // // return await this.repoUsecase.findEmail(params);
+    if (!passwordMatch) {
+      return new Error('Invalid email or password');
+    }else {
+      const accessToken = await JWTService.sign({uid: response.id});
+      return {token: accessToken, id: response.id};
+    }
   }
 }
