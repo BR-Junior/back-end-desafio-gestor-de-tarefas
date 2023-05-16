1694da6a4e38e913bb56d49398c045daf8c64813
BR-Junior <bianor.tech@gmail.com>
2023-05-16 18:48:23 -0300

    feat: add interface de caso de uso de UserCreate

---
+++ src/core/user/useCases/userUseCaseCreate/IUserUseCaseCreate.ts
@@ -0,0 +1,15 @@
+export interface IUserUseCaseCreate {
+  create(params: IUserUseCaseCreate.Params): Promise<IUserUseCaseCreate.Result | Error>
+}
+export namespace IUserUseCaseCreate {
+  export type Params = {
+    name: string;
+    email: string;
+    password: string;
+  }
+  export type Result = {
+    id: string
+    name: string;
+    email: string;
+  }
+}
