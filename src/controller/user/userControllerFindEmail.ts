1572036498ff95f035b8caf91c1e3043d8b62757
BR-Junior <bianor.tech@gmail.com>
2023-05-16 18:47:46 -0300

    feat: add userControllerFindEmail.ts de caso de uso de UserFindEmail(login)

---
+++ src/controller/user/userControllerFindEmail.ts
@@ -0,0 +1,13 @@
+import {Request, Response} from 'express';
+import {findEmail} from '../../core/user/useCases/userUseCaseFindEmail';
+
+export const userControllerFindEmail = async (req:Request, res: Response) => {
+  const params = req.body;
+
+
+  const result = await findEmail(params);
+  const {} = result;
+  if (result instanceof Error) return res.status(400).json(result.message);
+  return res.status(200).json({});
+
+};
