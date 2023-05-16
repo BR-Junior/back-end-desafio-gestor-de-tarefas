3415f5739f13f3d14e2d9b3188bfaa3044c53c73
BR-Junior <bianor.tech@gmail.com>
2023-05-16 18:49:59 -0300

    feat: add novas rotas de User

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,8 @@
 import {Router, Request, Response } from 'express';
 import { controllerUser } from  '../components/user';
+import {create} from '../core/user/useCases/userUseCaseCreate';
+import {userControllerCreate} from '../controller/user/userControllerCreate';
+import {userControllerFindEmail} from '../controller/user/userControllerFindEmail';
 
 const userRoutes = Router();
 userRoutes.post('/entrar'
@@ -10,6 +13,9 @@ userRoutes.post('/cadastrar', controllerUser.signUpValidation,
   (req:Request, res: Response) =>
     controllerUser.SignUp.execute(req, res));
 
+
+userRoutes.post('/user', (req:Request, res: Response) => userControllerCreate(req, res));
+userRoutes.post('/user-tra', (req:Request, res: Response) => userControllerFindEmail(req, res));
 
 
 export { userRoutes };
